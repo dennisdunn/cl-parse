4219374f84b0bd98ebcf3b9ae2a8e35b9983f9bc
Dennis Dunn <ansofive@gmail.com>
2023-05-11 00:57:31 -0400

    Added docstrings.

--- generators.lisp
+++ generators.lisp
@@ -3,12 +3,14 @@
 (in-package #:cl-tiny-parse)
 
 (defun str (str)
+    "Matches if the stream starts with str."
   (lambda (stream)
     (if (string= (peek stream (length str)) str)
         (values (read stream (length str)))
         nil)))
 
 (defun any-of-char (chars)
+    "Matches if the next character of stream is in chars."
   (lambda (stream)
     (if (search (peek stream) chars)
         (values (read stream))
